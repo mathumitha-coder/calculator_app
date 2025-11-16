c62ad99c54c139efc3f1a96a56225d78a9e7ea8b
mathumitha-coder <mathumitha.madan@gmail.com>
2025-11-16 16:21:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,13 +8,13 @@ x2 = st.number_input("Enter second number", value=0.0)
 operation = st.selectbox("Operations", ["Add", "Subtract", "Multiply", "Divide"])
 
 if st.button("Calculate"):
-    if operation == "Add":
+    if operation == "Add", x2 not 0:
         result = x1 + x2
     elif operation == "Subtract":
         result = x1 - x2
     elif operation == "Multiply":
         result = x1 * x2
-    elif operation == "Divide" if x2 not 0:
+    elif operation == "Divide":
         result = x1 / x2
     else:
         result = "you entered wrong number calculator developed by Mathumitha...!"
